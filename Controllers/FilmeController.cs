17f6913ce447ce68f4636dad60b90c5e648328a3
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-08-11 23:22:42 -0300

    Get e Post e boas práticas

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -11,11 +12,32 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         public static List<Filme> filmes = new List<Filme>();
-
+        public static int id = 1;
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme) {
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id}, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null) {
+                return Ok(filme);
+            } else {
+                return NotFound(filme);
+            }
         }
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,15 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+        
+        
+        [Required(ErrorMessage = "O campo \"Título\" é obrigatório.")]
         public string Titulo { get; set; }
-        
+        [Required(ErrorMessage = "O campo \"Diretor\" é obrigatório.")]
         public string Diretor { get; set; }
-        
+        [StringLength(30,ErrorMessage = "O gênero possui mais de 30 caracteres.")]
         public string Genero { get; set; }
-        
+        [Range(1,600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos.")]
         public int Duracao { get; set; }
-        
-        
     }
 }
